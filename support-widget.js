90b9ca559746f4e9d5a0060de0008d0c67a7516c
Turizam123 <nssr@abv.bg>
2025-08-11 20:01:55 +0300

    Update support-widget.js

--- support-widget.js
+++ support-widget.js
@@ -10,7 +10,7 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 const CONFIG = {
   SUPABASE_URL: "https://cmiylzpmpwqbacjoqtkx.supabase.co",
   SUPABASE_ANON: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNtaXlsenBtcHdxYmFjam9xdGt4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM5ODcwMDUsImV4cCI6MjA2OTU2MzAwNX0.FY2d1NSGTmw6SydxT_V0cKF7Kp2USbp91VdfO_eqZz8",
-  ADMIN_EMAIL: "82angeluzunov@gmail.com",
+  ADMIN_EMAIL: "uzunov.ange@gmail.com",
 };
 
 // Init
