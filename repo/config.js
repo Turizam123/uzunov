5e1e858666f14c3c3a87ff3998d61e157ec69099
Turizam123 <nssr@abv.bg>
2026-01-11 14:30:50 +0200

    Update config.js

--- repo/config.js
+++ repo/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  const SUPABASE_URL = "https://cmiylzpmpwqbacjoqtkx.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNtaXlsenBtcHdxYmFjam9xdGt4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM5ODcwMDUsImV4cCI6MjA2OTU2MzAwNX0.FY2d1NSGTmw6SydxT_V0cKF7Kp2USbp91VdfO_eqZz8";
- ADMIN_EMAIL: "uzunov.ange@gmail.com"
+  SUPABASE_URL: "https://cmiylzpmpwqbacjoqtkx.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNtaXlsenBtcHdxYmFjam9xdGt4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM5ODcwMDUsImV4cCI6MjA2OTU2MzAwNX0.FY2d1NSGTmw6SydxT_V0cKF7Kp2USbp91VdfO_eqZz8",
+  ADMIN_EMAIL: "uzunov.ange@gmail.com"
 };
