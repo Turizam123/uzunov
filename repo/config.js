e4861ebcb24f5809ea2a39569c377698c2035e33
Turizam123 <nssr@abv.bg>
2026-01-11 14:33:24 +0200

    Update config.js

--- repo/config.js
+++ repo/config.js
@@ -1,3 +1,5 @@
+// config.js — ЧИСТ JAVASCRIPT ФАЙЛ
+
 window.APP_CONFIG = {
   SUPABASE_URL: "https://cmiylzpmpwqbacjoqtkx.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNtaXlsenBtcHdxYmFjam9xdGt4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM5ODcwMDUsImV4cCI6MjA2OTU2MzAwNX0.FY2d1NSGTmw6SydxT_V0cKF7Kp2USbp91VdfO_eqZz8",
