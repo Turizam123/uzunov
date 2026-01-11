732abbc6ad1bfc20229bebeb67cef31d58e6d15f
Turizam123 <nssr@abv.bg>
2026-01-11 14:14:10 +0200

    Update config.js

--- repo/config.js
+++ repo/config.js
@@ -1,7 +1,5 @@
-const SUPABASE_URL = "https://cmiylzpmpwqbacjoqtkx.supabase.co";
+window.APP_CONFIG = {
+  const SUPABASE_URL = "https://cmiylzpmpwqbacjoqtkx.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNtaXlsenBtcHdxYmFjam9xdGt4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM5ODcwMDUsImV4cCI6MjA2OTU2MzAwNX0.FY2d1NSGTmw6SydxT_V0cKF7Kp2USbp91VdfO_eqZz8";
-
-const sb = supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
+ ADMIN_EMAIL: "uzunov.ange@gmail.com"
+};
